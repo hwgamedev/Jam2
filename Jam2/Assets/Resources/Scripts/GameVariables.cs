2fdd385f07a3bb12484ff4e0444765a6c5c5d6a2
plebras <pl196.hw@gmail.com>
2015-03-28 12:11:26 +0000

    gameVariable prefab

---
+++ Jam2/Assets/Resources/Scripts/GameVariables.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameVariables : MonoBehaviour {
+
+	public int TileSize = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
