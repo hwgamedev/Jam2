750989c29fe2974c6fe32cb76bcdd5f2c21f803a
plebras <pl196.hw@gmail.com>
2015-03-29 05:08:46 +0100

    character attacks

--- Jam2/Assets/Resources/Scripts/characterControls.cs
+++ Jam2/Assets/Resources/Scripts/characterControls.cs
@@ -5,22 +5,25 @@ public class characterControls : MonoBehaviour {
 
 	//public GameObject character;
 
+	private Animator anim;
 	public float speed = 1.0f;
 	private float startTime;
 	private float journeyLength;
 	private Vector3 startPosition;
 	private Vector3 endPosition;
 	private bool moving = false;
+	private bool attacking = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!moving) {
+		if (!moving && !attacking) {
 			if (Input.GetKey (KeyCode.W)) {
+				anim.SetTrigger("iddleN");
 				startTime = Time.time;
 				startPosition = transform.position;
 				endPosition = startPosition;
@@ -29,6 +32,7 @@ public class characterControls : MonoBehaviour {
 				moving = true;
 			}
 			if (Input.GetKey (KeyCode.A)) {
+				anim.SetTrigger("iddleE");
 				startTime = Time.time;
 				startPosition = transform.position;
 				endPosition = startPosition;
@@ -37,6 +41,7 @@ public class characterControls : MonoBehaviour {
 				moving = true;
 			}
 			if (Input.GetKey (KeyCode.S)) {
+				anim.SetTrigger("iddleS");
 				startTime = Time.time;
 				startPosition = transform.position;
 				endPosition = startPosition;
@@ -45,6 +50,7 @@ public class characterControls : MonoBehaviour {
 				moving = true;
 			}
 			if (Input.GetKey (KeyCode.D)) {
+				anim.SetTrigger("iddleW");
 				startTime = Time.time;
 				startPosition = transform.position;
 				endPosition = startPosition;
@@ -52,9 +58,37 @@ public class characterControls : MonoBehaviour {
 				journeyLength = Vector3.Distance (startPosition, endPosition);
 				moving = true;
 			}
+			if(Input.GetMouseButtonDown(0)){
+				Vector3 mousePoint = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+				float diffX = mousePoint.x - transform.position.x;
+				float diffY = mousePoint.y - transform.position.y;
+				if(diffX > -0.7 && diffX < 0.7){
+					if(diffY > 1 && diffY < 2){
+						anim.SetTrigger("attackN");
+						attacking = true;
+					}
+					else if(diffY < 0 && diffY > -1){
+						anim.SetTrigger("attackS");
+						attacking = true;
+					}
+				}
+				else if(diffY < 1 && diffY > 0){
+					if(diffX < -0.7 && diffX > -1.7){
+						anim.SetTrigger("attackE");
+						attacking = true;
+					}
+					else if(diffX > 0.7 && diffX < 1.7){
+						anim.SetTrigger("attackW");
+						attacking = true;
+					}
+				}
+			}
 		}
 		if (moving) {
 			doLerp ();
+		}
+		if (attacking) {
+			doAttack();
 		}
 	}
 
@@ -65,5 +99,9 @@ public class characterControls : MonoBehaviour {
 		if (transform.position == endPosition) {
 			moving = false;
 		}
+	}
+
+	private void doAttack(){
+		attacking = false;
 	}
 }
