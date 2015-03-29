a2a4659d9082b0e69ac02fe6ac338aa8017fbaad
plebras <pl196.hw@gmail.com>
2015-03-29 09:17:46 +0100

    fezihb

--- Jam2/Assets/Resources/Scripts/characterControls.cs
+++ Jam2/Assets/Resources/Scripts/characterControls.cs
@@ -5,6 +5,7 @@ public class characterControls : MonoBehaviour {
 
 	//public GameObject character;
 	public GameVariables vars;
+
 	private Animator anim;
 	public int steps = 20;
 	public int health = 20;
@@ -21,6 +22,7 @@ public class characterControls : MonoBehaviour {
 	void Start () {
 		anim = GetComponent<Animator>();
 		vars = (GameVariables) GameObject.FindWithTag("GameVariables").GetComponent("GameVariables");
+		
 	}
 	
 	// Update is called once per frame
@@ -39,7 +41,7 @@ public class characterControls : MonoBehaviour {
 					endPosition = startPosition;
 					endPosition += new Vector3 (0, 1, 0);
 					journeyLength = Vector3.Distance (startPosition, endPosition);
-					steps --;
+					doStep();
 					moving = true;
 				}
 				if (Input.GetKey (KeyCode.A)) {
@@ -49,7 +51,7 @@ public class characterControls : MonoBehaviour {
 					endPosition = startPosition;
 					endPosition -= new Vector3 (1, 0, 0);
 					journeyLength = Vector3.Distance (startPosition, endPosition);
-					steps --;
+					doStep();
 					moving = true;
 				}
 				if (Input.GetKey (KeyCode.S)) {
@@ -59,7 +61,7 @@ public class characterControls : MonoBehaviour {
 					endPosition = startPosition;
 					endPosition -= new Vector3 (0, 1, 0);
 					journeyLength = Vector3.Distance (startPosition, endPosition);
-					steps --;
+					doStep();
 					moving = true;
 				}
 				if (Input.GetKey (KeyCode.D)) {
@@ -69,7 +71,7 @@ public class characterControls : MonoBehaviour {
 					endPosition = startPosition;
 					endPosition += new Vector3 (1, 0, 0);
 					journeyLength = Vector3.Distance (startPosition, endPosition);
-					steps --;
+					doStep();
 					moving = true;
 				}
 				if (Input.GetMouseButtonDown (0)) {
@@ -79,13 +81,13 @@ public class characterControls : MonoBehaviour {
 					if (diffX > -0.7 && diffX < 0.7) {
 						if (diffY > 1 && diffY < 2) {
 							anim.SetTrigger ("attackN");
-							steps --;
+							doStep();
 							trace.transform.rotation = Quaternion.Euler(0,0,89f);
 							trace.Play();
 							attacking = true;
 						} else if (diffY < 0 && diffY > -1) {
 							anim.SetTrigger ("attackS");
-							steps --;
+							doStep();
 							trace.transform.rotation = Quaternion.Euler(0,0,-91f);
 							trace.Play();
 							attacking = true;
@@ -93,17 +95,16 @@ public class characterControls : MonoBehaviour {
 					} else if (diffY < 1 && diffY > 0) {
 						if (diffX < -0.7 && diffX > -1.7) {
 							anim.SetTrigger ("attackE");
-							steps --;
+							doStep();
 							trace.transform.rotation = Quaternion.Euler(0,0,-179f);
 							trace.Play();
 							attacking = true;
 						} else if (diffX > 0.7 && diffX < 1.7) {
 							anim.SetTrigger ("attackW");
-							steps --;
+							doStep();
 							trace.transform.rotation = Quaternion.Euler(0,0,-1f);
 							trace.Play();
 							attacking = true;
-							doDammages (5);
 						}
 					}
 				}
@@ -115,6 +116,11 @@ public class characterControls : MonoBehaviour {
 				doAttack ();
 			}
 		}
+	}
+
+	private void doStep(){
+		steps --;
+		// notify game mechanic of step performed by player
 	}
 
 	private void doLerp() {
@@ -130,7 +136,7 @@ public class characterControls : MonoBehaviour {
 		// TODO proper collision detection with enemies
 		//RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.right, 5, <enemy layer>);
 		//if (hit.collider != null) {
-		//	print ("hit");
+		//	
 		//}
 		if (anim.GetCurrentAnimatorStateInfo (0).IsName ("iddleN") ||
 			anim.GetCurrentAnimatorStateInfo (0).IsName ("iddleS") ||
