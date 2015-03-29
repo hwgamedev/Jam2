8fac8dab2942ff7d3bcb17afce42c2e6ba3e24b7
mindaugaskadzys <mindaugas.kadzys@gmail.com>
2015-03-29 14:23:51 +0100

    Treasure chests/obstacles added

---
+++ Jam2/Assets/Resources/Scripts/OpenTreasure.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenTreasure : MonoBehaviour {
+
+    public GameObject[] prizes;
+    private bool claimed = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            spawnPrize();
+
+        }
+    }
+
+    public void spawnPrize()
+    {
+        if (!claimed)
+        {
+            GetComponent<Animator>().SetTrigger("OpenChest");
+            claimed = true;
+            print("Spawning prize!");
+            GameObject prize = Instantiate(prizes[Random.Range(0, prizes.Length)], new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+            prize.transform.position = new Vector2(transform.position.x, transform.position.y);
+            if (prize.name == "GoldStack")
+            {
+                prize.GetComponent<GoldPickup>().collect(Random.Range(50, 150));
+            }
+        }
+    }
+}
